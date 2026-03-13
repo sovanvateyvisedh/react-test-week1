34dacdd934628949aca269916f4ce13dcf08f7fe
Vatey <160231919+sovanvateyvisedh@users.noreply.github.com>
2026-03-13 15:09:59 +0700

    feat: initial commit

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,15 +1,7 @@
 import React from 'react'
-interface User {
-  id: number;
-  name: string;
-  age: number;
-  status: 'single' | 'married';  
-  img: string;
-}
-function UserContext() {
+
+export default function UserContext() {
   return (
     <div>UserContext</div>
   )
 }
-
-export default UserContext
